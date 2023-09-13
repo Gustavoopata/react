b190d31d569217d7dca54c7ec47ec95343a8f472
Gustavoopata <gustavobimbinha24@gmail.com>
2023-09-13 18:17:38 -0300

    first commit

---
+++ react_2/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+// return tudo q vier dentro do return vai ser o html somente 
+  return (
+    <>
+ <h2>feliz dia do programador</h2>
+
+    </>
+  )
+}
+
+export default App
